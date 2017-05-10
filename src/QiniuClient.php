990429c38fb0ba845e4bd20cf519d92a7f353dad
liujun <liujun@liujundeMacBook-Pro.local>
2017-05-10 23:07:06 +0800

    fix bug

--- src/QiniuClient.php
+++ src/QiniuClient.php
@@ -4,12 +4,15 @@ namespace TJDS\Lib;
 use Qiniu\Auth;
 use Qiniu\Storage\UploadManager;
 
+use Qiniu\Auth;
+use Qiniu\Storage\UploadManager;
+
 class QiniuClient
 {
     const AK= "uK5hYhgItfyK7FrStqgTX3W61S8c1ztohngHZfFS";
     const SK= "qWy-Bl9hMx3FwEv_PL8cpcMiuYzXlT_-UrXd5rQ-";
 
-    public function put($bucket, $upPath, $stream){
+    public static function put($bucket, $upPath, $stream){
         // 需要填写你的 Access Key 和 Secret Key
         $accessKey = self::AK;
         $secretKey = self::SK;
@@ -22,16 +25,11 @@ class QiniuClient
         $uploadMgr = new UploadManager();
 
         // 调用 UploadManager 的 putFile 方法进行文件的上传
-        list($ret, $err) = $uploadMgr->put($token, $upPath, $stream);
-        echo "\n====> putFile result: \n";
-        if ($err !== null) {
-            var_dump($err);
-        } else {
-            var_dump($ret);
-        }
+        return $uploadMgr->put($token, $upPath, $stream);
+
     }
 
-    public function putFile($bucket, $upPath, $filePath){
+    public static function putFile($bucket, $upPath, $filePath){
         // 需要填写你的 Access Key 和 Secret Key
         $accessKey = self::AK;
         $secretKey = self::SK;
@@ -43,12 +41,6 @@ class QiniuClient
         // 初始化 UploadManager 对象并进行文件的上传
         $uploadMgr = new UploadManager();
 
-        list($ret, $err) = $uploadMgr->putFile($token, $upPath, $filePath);
-        echo "\n====> putFile result: \n";
-        if ($err !== null) {
-            var_dump($err);
-        } else {
-            var_dump($ret);
-        }
+        return $uploadMgr->putFile($token, $upPath, $filePath);
     }
 }
